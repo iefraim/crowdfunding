c023e503a132d8d9736ae8a27c334df7e70370c7
Devorah Fleisher <devorah@cucumber.com>
2023-07-09 18:07:22 -0600

    mommys design

--- src/components/LeftColumn.js
+++ src/components/LeftColumn.js
@@ -5,12 +5,11 @@ import Teams from "./Teams";
 
 const LeftColumn = (props) => (
   <div className="leftColumn col-md-8 col-xs-12 col-md-pull-4 ">
-    <div className="topimage container-fluid">
+    <div className="topimage container-fluid"></div>
+    <div className="donatebox donors">
       <About />
+      <DonationPresets />
     </div>
-
-    <DonationPresets />
-
     <Teams />
   </div>
 );
