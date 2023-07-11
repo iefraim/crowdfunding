906ee99d0a55c2ce5dc58b2337e7a539b302c0eb
Devorah Fleisher <devorah@cucumber.com>
2023-07-11 15:45:57 -0600

    set up teams outline

---
+++ src/components/Team.js
@@ -0,0 +1,5 @@
+import React from "react";
+//TODO
+const Team = () => <div>Team</div>;
+
+export default Team;
--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,5 +1,27 @@
-import React from "react";
-//TODO
-const Teams = () => <div>Teams</div>;
+import React, { useContext } from "react";
+
+import Team from "./Team";
+
+import { TeamContext } from "../context/Provider";
+//TODO:style
+const Teams = () => {
+  const teams = useContext(TeamContext);
+  return (
+    <div className="donatebox teams">
+      <div className="row">
+        <div className="col-12">
+          <div className="title-box">{teams.length} teams</div>
+          <div id="teams">
+            <ul id="teams-list" className="row">
+              {teams.map((item) => (
+                <Team key={item.id} item={item} />
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Teams;
