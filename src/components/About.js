f975a74e972e5babfdf1146f4e6e60059ee3ef7f
Devorah Fleisher <devorah@cucumber.com>
2023-07-11 09:46:44 -0600

    about

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,20 @@
 import React from "react";
-//TODO
-const About = () => <div>About</div>;
+//TODO:styling
+const About = () => {
+  return (
+    <div>
+      <h3>ABOUT ZERA AVRAHAM</h3>
+      <div>
+        Congregation Zera Abraham is well defined by its byline: Warm and
+        Authentic Torah Living for Over a Century. It is a Shul- strongly Torah
+        oriented, and a community of Torah Living. With vibrant minyanim,
+        excellent shiurim and exciting programs, the Shul continues, year after
+        year, one century through another, to provide enjoyable Yiddishkeit and
+        opportunities for continued personal and collective growth, all while
+        maintaining its age old authenticity.
+      </div>
+    </div>
+  );
+};
 
 export default About;
