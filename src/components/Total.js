f457041709f2078ff5ab982abcdc78a91cb4a902
Devorah Fleisher <devorah@cucumber.com>
2023-07-12 15:11:14 -0600

    set up total donations display

--- src/components/Total.js
+++ src/components/Total.js
@@ -1,5 +1,41 @@
-import React from "react";
+import React, { useContext } from "react";
+import ProgressBar from "@ramonak/react-progress-bar";
+
+import { DonationContext, DataContext } from "../context/Provider";
 //TODO
-const Total = () => <div>Total</div>;
+const Total = () => {
+  const donations = useContext(DonationContext);
+  const { goal, bonus_goal } = useContext(DataContext);
+  const totalDonations = donations.reduce(
+    (prev, curr) => prev + parseInt(curr.amount),
+    0
+  );
+
+  return (
+    <>
+      {totalDonations < goal && (
+        <div>
+          <div className="totalAmount">${totalDonations.toLocaleString()}</div>{" "}
+          of
+          <div className="goal"> ${parseInt(goal).toLocaleString()} </div>
+        </div>
+      )}
+
+      {totalDonations >= goal && totalDonations < bonus_goal && (
+        <div>
+          <h5>NEW GOAL!</h5>
+          <h6>
+            ${totalDonations.toLocaleString()} out of $
+            {parseInt(bonus_goal).toLocaleString()} raised!
+          </h6>
+        </div>
+      )}
+      {totalDonations >= bonus_goal && (
+        <h6>Goal reached with ${totalDonations.toLocaleString()} raised!</h6>
+      )}
+      <ProgressBar completed={Math.round((totalDonations / goal) * 100)} />
+    </>
+  );
+};
 
 export default Total;
