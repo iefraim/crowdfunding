e81fc068bee8abe211db2ab17319d40509efd993
debs2830 <devorah@cucumber.com>
2023-12-17 19:12:59 -0700

    new images

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 const Header: React.FC = () => (
-  <section className="jumbotron text-center header__image"></section>
+
+    <>
+  <section className="jumbotron text-center header__image ">
+
+          </section>
+    </>
+
 );
 
 export default Header;
